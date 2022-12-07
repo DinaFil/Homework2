9fc1d58d8941a00f303030d79e8852a0af07bff7
Dina Filippova <dinafilippova@gmail.com>
2022-12-07 17:16:54 +0700

    Task016

---
+++ Task016/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Пожалуйста, введите число 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Пожалуйста, введите число 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int x = number1/number2;
+int y = number2/number1;
+if (x == number2 || y == number1) Console.WriteLine("Одно число является квадратом другого");
+else Console.WriteLine("Одно число не является квадратом другого");
