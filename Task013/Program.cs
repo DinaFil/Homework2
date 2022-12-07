0bcf352059da523e4bb220cc04e195195c136cd6
Dina Filippova <dinafilippova@gmail.com>
2022-12-07 16:36:29 +0700

    Task013

---
+++ Task013/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите любое число: ");
+string ABC = Console.ReadLine();
+int x = ABC.Length;
+if (x < 3) Console.WriteLine ("В заданном числе отсутсвует третья цифра");
+else Console.WriteLine(ABC[2]);
