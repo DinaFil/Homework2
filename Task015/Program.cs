990ef97db2ad455d27e506d633c13d6af42c729d
Dina Filippova <dinafilippova@gmail.com>
2022-12-07 17:03:46 +0700

    Task015

---
+++ Task015/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число, означающее день недели");
+int Day = Convert.ToInt32(Console.ReadLine());
+int[] Days = {1,2,3,4,5,6,7};
+if (Day > Days.Length) Console.WriteLine("Введенное число не соответствует дню недели");
+else
+{
+if (Day > Days[4]) Console.WriteLine("Ура, это выходной!");
+else Console.WriteLine("Идем на работу");
+}
